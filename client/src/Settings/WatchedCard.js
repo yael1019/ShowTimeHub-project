4d1898e3b83963137c709e32877e021299a0249c
Yael Harosh <yaelharosh01@gmail.com>
2023-06-16 09:07:36 -0400

    video info pg from watched pg

--- client/src/Settings/WatchedCard.js
+++ client/src/Settings/WatchedCard.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function WatchedCard({ video }) {
+  const navigate = useNavigate()
+  function handleClick() {
+    navigate('/videoInfo', {state: video})
+  }
   return (
     <div>
-        <img src={ video.image } alt='poster' />
+        <img src={ video.image } alt='poster' onClick={handleClick}/>
     </div>
   )
 }
