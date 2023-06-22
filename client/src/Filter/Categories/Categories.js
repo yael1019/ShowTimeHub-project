d9b386597ad16c8e546d721d9b07c1257201bd86
Yael Harosh <yaelharosh01@gmail.com>
2023-06-22 11:13:34 -0400

    category page css

--- client/src/Filter/Categories/Categories.js
+++ client/src/Filter/Categories/Categories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../filter.css'
 import { FiArrowLeft } from 'react-icons/fi'
 import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 function Categories() {
   const navigate = useNavigate()
@@ -12,15 +13,40 @@ function Categories() {
   return (
     <div>
       <p className='exit' onClick={() => navigate('/')}><FiArrowLeft/></p>
-      <form>
-        <button value='Drama' onClick={handleClick}>Drama</button>
-        <button value='Reality TV' onClick={handleClick}>Reality TV</button>
-        <button value='Thrillers' onClick={handleClick}>Thrillers</button>
-        <button value='Comedy' onClick={handleClick}>Comedy</button>
-        <button value='Action & Adventure' onClick={handleClick}>Action & Adventure</button>
-        <button value='Sci-Fi TV' onClick={handleClick}>Sci-Fi TV</button>
-        <button value='Fantasy' onClick={handleClick}>Fantasy</button>
-      </form>
+      <motion.form
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{duration: 1.5, delay: 0.5}}
+      id='category-form'>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Drama' onClick={handleClick}>Drama</motion.button>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Reality TV' onClick={handleClick}>Reality TV</motion.button>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Thrillers' onClick={handleClick}>Thrillers</motion.button>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Comedy' onClick={handleClick}>Comedy</motion.button>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Action & Adventure' onClick={handleClick}>Action & Adventure</motion.button>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Sci-Fi TV' onClick={handleClick}>Sci-Fi TV</motion.button>
+        <motion.button 
+        whileHover={{scaleY: 1.1}}
+        whileTap={{backgroundColor: 'black', scale: 0.9}}
+        className='cat-button' value='Fantasy' onClick={handleClick}>Fantasy</motion.button>
+      </motion.form>
     </div>
   )
 }
