d9acaa12e870adfd1330079f96ca3533edb0a74f
Yael Harosh <yaelharosh01@gmail.com>
2023-06-22 14:56:23 -0400

    search transition

--- client/src/Search/Search.js
+++ client/src/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Search.css'
 import { CgSearch } from 'react-icons/cg'
+import { motion } from 'framer-motion'
 
 function Search({ searching, setSearching }) {
   const [search, setSearch] = useState(false)
@@ -11,18 +12,53 @@ function Search({ searching, setSearching }) {
     setSearching(e.target.value)
   }
   return (
-    <div>
-      {
+    <motion.div 
+    
+    className='search-bar'>
+
+
+      {/* {
         !search
         ?
         <p id='searchIcon' onClick={handleClick}><CgSearch /></p>
         :
-        <>
-          <p id='searchIcon' onClick={handleClick}><CgSearch /></p>
-          <input type='text' placeholder='search' value={ searching } onChange={ handleChange }/>
-        </>
-      }
-    </div>
+        <motion.div initial={{opacity: 0}} 
+        animate={{opacity: 1, width: search ? 200 : 2}} 
+        transition={{duration: 1.5}}>
+          <p id='searchIcon' onClick={handleClick}><CgSearch color='black'/></p>
+          <motion.input 
+          id='search-box' type='text' placeholder='search' value={ searching } onChange={ handleChange }/>
+        </motion.div>
+      } */}
+
+
+
+        <motion.p 
+        animate={{
+          backgroundColor: search ? "transparent" : "red",
+          color: search ? "black" : "white",
+          transition: {
+            delay: search ? 0 : 0.5
+          }
+        }}
+        id='searchIcon' onClick={handleClick}><CgSearch/></motion.p>
+        <motion.input 
+        animate={{
+          width: search ? 200 : 0,
+          // backgroundColor: search ? "white" : "transparent",
+          transition: {
+            duration: 0.5,
+            ease: "circOut"
+          }
+        }}
+        exit={{
+          transition: {
+            duration: 0.5,
+            ease: "circIn"
+          }
+        }}
+         id='search-box' type='text' placeholder='search' value={ searching } onChange={ handleChange }/>
+    </motion.div>
   )
 }
 
