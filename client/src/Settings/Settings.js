817860568c67c05cafb4bd35c3fc380377b3832d
Yael Harosh <yaelharosh01@gmail.com>
2023-06-28 14:10:30 -0400

    settings page

--- client/src/Settings/Settings.js
+++ client/src/Settings/Settings.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Settings.css'
 import { FiArrowLeft } from 'react-icons/fi'
 import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 
 function Settings({ currentUser, handleLogout }) {
@@ -12,12 +13,23 @@ function Settings({ currentUser, handleLogout }) {
   return (
     <div>
         <p onClick={() => navigate('/')}><FiArrowLeft/></p>
-        <h2>{ currentUser.first_name }</h2>
-        <h2>{ currentUser.last_name }</h2>
-        <h2>{ currentUser.email }</h2>
-        <h2>password: *******</h2>
-        <p onClick={handleWatched}>Watched</p>  
-        <button onClick={ handleLogout }>Logout</button>
+        <motion.div
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{duration: 1.5, delay: 0.5}}
+        className='settingDiv'>
+          <h2 className='info'>{ currentUser.first_name }</h2>
+          <h2 className='info'>{ currentUser.last_name }</h2>
+          <h2 className='info'>{ currentUser.email }</h2>
+          <h2 className='info'>password: *******</h2>
+          <motion.p
+          whileHover={{x: 20}}
+          onClick={handleWatched} className='info watched'>Your Watched</motion.p>  
+          <motion.button
+          whileHover={{scaleY: 1.1}}
+          whileTap={{backgroundColor: 'black', scale: 0.9}}
+          onClick={ handleLogout } id='logout'>Logout</motion.button>
+        </motion.div>
     </div>
   )
 }
